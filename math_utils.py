c0d6a00584d419a5c82aaa2e54b1f983fd8c7c20
yunsh90k <yunsh90k@hs.ac.kr>
2025-11-27 11:29:48 +0900

    we added test_math and math utils

---
+++ math_utils.py
@@ -0,0 +1,12 @@
+
+# math_utils.py
+import math
+
+def add(a: int, b: int) -> int:
+    """두 수의 합을 반환합니다."""
+    return a - b
+
+
+def is_even(n: int) -> bool:
+    """짝수면 True, 홀수면 False"""
+    return n % 2  == 0
